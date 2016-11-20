590a0316ec9c9dbe17ea2dcb7d6921bffba62df6
peter kentish <pkentishdelaiglesia1@sheffield.ac.uk>
2016-11-20 02:38:11 +0100

    Scrolly mc scrollface

--- scroll.js
+++ scroll.js
@@ -2,9 +2,7 @@ var scroll_down_flag = 0;
 var scroll_up_flag = 0;
 var start_time;
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+
 window.addEventListener("message", function(event) {
     if (event.source != window)
         return;
@@ -19,7 +17,7 @@ window.addEventListener("message", function(event) {
 
 
 function start(webgazerData) {
-    webgazer.setRegression('ridge') /* currently must set regression and tracker */
+    webgazer//.setRegression('ridge') /* currently must set regression and tracker */
         .setTracker('clmtrackr')
         .setGazeListener(function(data, clock) {
             //If data and looking within screen boundaries
@@ -40,8 +38,8 @@ function start(webgazerData) {
                     {
                         console.log("1 second passed; scrolling down");
                         console.log(window.scrollY);
-                        window.scrollBy(0, (data.y/window.innerHeight)*10)
                         var x = window.innerHeight *0.25
+                        window.scrollBy(0, (((data.y-window.innerHeight*0.75)/x)*10));
                         console.log(((data.y-window.innerHeight*0.75)/x)*10);
                     }
                 }
@@ -71,5 +69,57 @@ function start(webgazerData) {
             }                
         })
   .begin(webgazerData)
-    .showPredictionPoints(true); /* shows a square every 100 milliseconds where current prediction is */
+  .showPredictionPoints(true); /* shows a square every 100 milliseconds where current prediction is */
+var width = 320;
+    var height = 240;
+    var topDist = '0px';
+    var leftDist = '0px';
+    
+    var setup = function() {
+        var video = document.getElementById('webgazerVideoFeed');
+        video.style.display = 'block';
+        video.style.position = 'absolute';
+        video.style.top = topDist;
+        video.style.left = leftDist;
+        video.width = width;
+        video.height = height;
+        video.style.margin = '0px';
+
+        webgazer.params.imgWidth = width;
+        webgazer.params.imgHeight = height;
+
+        var overlay = document.createElement('canvas');
+        overlay.id = 'overlay';
+        overlay.style.position = 'absolute';
+        overlay.width = width;
+        overlay.height = height;
+        overlay.style.top = topDist;
+        overlay.style.left = leftDist;
+        overlay.style.margin = '0px';
+
+        document.body.appendChild(overlay);
+
+        var cl = webgazer.getTracker().clm;
+
+        function drawLoop() {
+            requestAnimFrame(drawLoop);
+            overlay.getContext('2d').clearRect(0,0,width,height);
+            if (cl.getCurrentPosition()) {
+                cl.draw(overlay);
+            }
+        }
+        drawLoop();
+    };
+
+    function checkIfReady() {
+        if (webgazer.isReady()) {
+            setup();
+        } else {
+            setTimeout(checkIfReady, 100);
+        }
+    }
+    setTimeout(checkIfReady,100);
+
+console.log(webgazer.getRegression())
 }
+
